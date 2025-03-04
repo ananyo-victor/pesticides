d4135fead74f1ff50549d1c03d2785fabc081f34
disha3110 <dishaagrawal3110@gmail.com>
2025-03-04 16:00:59 +0530

    Add job retrieval by ID and enhance job posting response

--- src/routes/HR_Routes.js
+++ src/routes/HR_Routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { createJobPost } from "../controllers/HR_Controllers/jobPostController.js"; 
-import { getJobs } from "../controllers/HR_Controllers/getJobsController.js";
+import { getJobs, getJobsById } from "../controllers/HR_Controllers/getJobsController.js";
 
 const router = express.Router();
 
 router.post('/postjob', createJobPost);
 router.get('/getjobs', getJobs);
+router.get('/getjobsById/:Id', getJobsById);
 
 
 export default router;
