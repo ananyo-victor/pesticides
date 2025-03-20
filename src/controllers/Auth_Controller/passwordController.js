30f4d5bea855dc065a69e45cf2ee7167bed472f9
RuhaniAsija <ruhaniasija@gmail.com>
2025-03-20 16:39:58 +0530

    pass

--- src/controllers/Auth_Controller/passwordController.js
+++ src/controllers/Auth_Controller/passwordController.js
@@ -2,7 +2,7 @@
 import { forgotPassword, resetPassword } from "../../service/authServices/passwordService.js";
 export const forgotPasswordC = async (req, res) => {
     try {
-        const message = await forgotPassword(req.body.email);
+        const message = await forgotPassword(req.body.email, req);
         res.json({ message });
     } catch (error) {
         res.status(400).json({ error: error.message });
