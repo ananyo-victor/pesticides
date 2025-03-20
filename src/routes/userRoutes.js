917e95890768714bd2966701ea73dd03db70e110
ananyo-victor <ananyomaitroan@gmail.com>
2025-03-20 10:26:44 +0530

    Add functionality to retrieve applied job details by ID; update job controller and routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getUserProfile, updateUserProfile } from "../controllers/User_Controller/userController.js";
-import { getJobs, getJobById } from "../controllers/User_Controller/jobController.js";
+import { getJobs, getJobById, getAppliedJobById } from "../controllers/User_Controller/jobController.js";
 import { applyForJob, isJobApplied } from "../controllers/User_Controller/applicationController.js";
 import { verifyTokens } from "../middlewares/verifyTokens.js";
 import { upload } from "../middlewares/multer/multerFile.js";
@@ -13,6 +13,7 @@ router.get('/profile/', verifyTokens, getUserProfile);
 router.put('/upProfile/',verifyTokens, upload, updateUserProfile);
 router.get("/getjobs", getJobs);
 router.get("/getjobsById/:Id", verifyTokens, getJobById);
+router.get("/getAppliedJobsById/:Id", getAppliedJobById);
 router.post('/profile/application/:jobId', verifyTokens, upload, applyForJob);
 router.get('/profile/appliedjobs/myjob', verifyTokens, AppliedJobs);
 router.get('/profile/appliedjobs/check', verifyTokens, isJobApplied);
